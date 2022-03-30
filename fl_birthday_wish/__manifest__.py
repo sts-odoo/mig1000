2ec60de5503e7a06dc647924e9fed41567162b7b
Stanislas Sobieski <sts@odoo.com>
2022-03-30 12:00:32 +0200

    modulev

--- fl_birthday_wish/__manifest__.py
+++ fl_birthday_wish/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Birthday Wishes',
-    'version': '14.0.9.0.6' ,
+    'version': '14.0.9.0.4',
     'category': 'Contacts',
     'summary': 'Birthday Wishes for Employee, Customer',
     'description': """
