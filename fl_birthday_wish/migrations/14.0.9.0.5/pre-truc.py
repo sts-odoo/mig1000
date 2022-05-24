4f0a4f2821dda9d3b167b15b6ba18ca0d4c5070b
Stanislas Sobieski <sts@odoo.com>
2022-05-24 10:57:37 +0200

    Update pre-truc.py

--- fl_birthday_wish/migrations/14.0.9.0.5/pre-truc.py
+++ fl_birthday_wish/migrations/14.0.9.0.5/pre-truc.py
@@ -8,5 +8,5 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
 
     _logger.info("ca migreeeeeuxxxx.........")
-    time.sleep(600)
+    #time.sleep(600)
     _logger.error('ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION')
